ae027f627800bfdfa58a47cf31e8b5ad2a3b1e62
Ольга Тарасова <olga_filippova@bk.ru>
2023-02-01 08:52:47 +0300

    Добавлены модели Ревью, Коммент, сериализаторы, вью для них

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,6 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
-from reviews.models import Category, Genre, Title
+from reviews.models import Category, Comment, Genre, Review, Title
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -21,3 +22,44 @@ class TitleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Title
         fiels = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    title = serializers.SlugRelatedField(
+        slug_field='name',
+        read_only=True
+    )
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+
+    class Meta:
+        model = Review
+        fields = ('id', 'text', 'author', 'score', 'pub_date')
+
+    def validate(self, data):
+        title_id = self.context['view'].kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        if self.context['request'].method == 'POST':
+            if Review.objects.filter(
+                author=self.context['request'].user, title=title
+            ).exists():
+                raise serializers.ValidationError(
+                    'Вы уже оставля свой отзыв к этому произведению'
+                )
+        return data
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'text', 'author', 'pub_date')
+        read_only = ('review',)  
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,12 +1,18 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import (CategoryViewSet, GenreViewSet, TitleViewSet)
+from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
+                    ReviewViewSet, TitleViewSet)
 
 
 v1_router = routers.DefaultRouter()
 v1_router.register('genres', GenreViewSet, basename='genres')
 v1_router.register('categories', CategoryViewSet, basename='categories')
 v1_router.register('titles', TitleViewSet, basename='titles')
+v1_router.register(
+    r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='reviews')
+v1_router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('v1/', include(v1_router.urls)),
