e43ec1dfff38b77cff820438e2955338dfe1adf3
Владислав Шкаровский <vladislav.shkarovskiy@yandex.ru>
2023-02-04 19:41:53 +0300

    debuged csv_script

--- api_yamdb/core/management/commands/csv_to_db.py
+++ api_yamdb/core/management/commands/csv_to_db.py
@@ -1,35 +1,28 @@
 import csv
 import os
+import sys
 
+from colorama import Fore
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from reviews.models import Category, Comment, Genre, Review, Title
 from users.models import User
 
 FILE_TO_MODELS = [
-    ('category.csv', Category),
-    #('comments.csv', Comment),
-    ('genre.csv', Genre),
-    #('genre_title.csv', Title.genre.through),
-    #('review.csv', Review),
-    ('titles.csv', Title),
     ('users.csv', User),
+    ('category.csv', Category),
+    ('titles.csv', Title),
+    ('genre.csv', Genre),
+    ('genre_title.csv', Title.genre.through),
+    ('review.csv', Review),
+    ('comments.csv', Comment),
 ]
 
 
 class Command(BaseCommand):
+    help = 'Upload data from csv file to sql database'
 
-    # def handle(self, *args, **kwargs):
-
-    #     for csv_file, model in FILE_TO_MODELS:
-    #         location = os.path.join(settings.CSV_DATA_DIR, csv_file)
-    #         fff = pathlib.PurePath(location)
-    #         open(location)
-    #         self.stdout.write(
-    #             f'Модель: {model}; Путь: {location}'
-    #         )
-
-    def handle(self, *args, **options):
+    def handle(self, **options):
 
         if options['clear']:
             for csv_file, model in FILE_TO_MODELS:
@@ -37,7 +30,9 @@ class Command(BaseCommand):
 
         for csv_file, model in FILE_TO_MODELS:
             import_csv_data(
-                os.path.join(settings.CSV_DATA_DIR, csv_file), model
+                self,
+                os.path.join(settings.CSV_DATA_DIR, csv_file),
+                model
             )
 
     def add_arguments(self, parser):
@@ -46,11 +41,11 @@ class Command(BaseCommand):
             '--clear',
             action='store_true',
             default=False,
-            help='Clear model befor import'
+            help='Clear before import'
         )
 
 
-def import_csv_data(csv_file, model):
+def import_csv_data(self, csv_file, model):
     with open(csv_file, encoding='utf-8') as csv_model_data:
         csv_reader = csv.reader(csv_model_data, delimiter=',')
         fields = next(csv_reader)
@@ -64,3 +59,6 @@ def import_csv_data(csv_file, model):
                 field: row[count] for count, field in enumerate(fields)
             }
             _, created = model.objects.get_or_create(**value)
+            if not created:
+                sys.exit(Fore.RED + 'CSV to database record - ERORR!!!')
+            self.stdout.write(Fore.GREEN + f'Recorded: {created}\n{value}\n\n')
