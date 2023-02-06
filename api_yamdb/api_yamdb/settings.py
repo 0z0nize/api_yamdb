4149cd7439317a9300ecac3d5bd2e891978780b0
Владислав Шкаровский <vladislav.shkarovskiy@yandex.ru>
2023-02-06 14:01:38 +0300

    settings update

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,7 +23,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'djoser',
     'django_filters',
     'reviews.apps.ReviewsConfig',
     'api.apps.ApiConfig',
