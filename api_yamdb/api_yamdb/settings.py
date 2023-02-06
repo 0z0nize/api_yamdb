1139b17e9c5c3877097904f5da218ecb93150260
Владислав Шкаровский <vladislav.shkarovskiy@yandex.ru>
2023-02-06 13:31:05 +0300

    djoser in installd app

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'djoser',
     'django_filters',
     'reviews.apps.ReviewsConfig',
     'api.apps.ApiConfig',
